49d437e814c503efdf5058d278bb05a5b6be1472
bao-daika <56355940+bao-daika@users.noreply.github.com>
2020-04-06 22:30:06 -0400

    Add files via upload

--- function.js
+++ function.js
@@ -4,5 +4,23 @@ function myFunction() {
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
   var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
+  document.getElementById("myBar").style.height = scrolled + "%";
+  if(scrolled === 100){
+    window.alert('Congratulation, you finished all programs!')
+  }
 }
+
+$('a').click(function () { 
+ 
+  if($('a').hasClass("box")){
+    $('a').removeClass("box");
+  };
+  if($(this).attr('href') !== "#home"){
+    $(this).addClass("box");
+    
+  }
+   
+});
+
+
+$(`#article.img`).removeClass(`hover`)
